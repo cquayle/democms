99d16757fe661bd76834785e6a42b76aa81f3870
Chris Quayle <cquayle@poppulo.com>
2026-01-24 17:14:59 +0000

    RI-419: Graph API configs

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default () => ({
+    graphql: {
+        config: {
+          endpoint: '/graphql',
+          shadowCRUD: true,
+          landingPage: true,
+          depthLimit: 3,
+          amountLimit: 100,
+          apolloServer: {
+            tracing: true,
+            introspection: true
+          },
+        },
+      },
+});
